b00a1371d3b574258e461e07ee6d2140eba74b62
farooq <farooquit21@gmail.com>
2022-02-03 17:58:53 +0500

    showing lat and lng

--- ATAM/tracking/routing.py
+++ ATAM/tracking/routing.py
@@ -2,5 +2,5 @@ import imp
 from django.urls import path
 from .consumers import WSConsumer
 ws_urlpatterns = [
-    path('ws/some_url/',WSConsumer.as_asgi())
+    path('ws/tracking/',WSConsumer.as_asgi())
 ]
--- ATAM/tracking/static/tracking/js/script.js
+++ ATAM/tracking/static/tracking/js/script.js
@@ -1,3 +1,5 @@
+
+
 // let map;
 // function initMap() {
 //   const mapOptions = {
@@ -13,20 +15,20 @@
 //     position: { lat: 24.9180, lng: 67.0971 },
 //     map: map,
 //   });
-// You can use a LatLng literal in place of a google.maps.LatLng object when
-// creating the Marker object. Once the Marker object is instantiated, its
-// position will be available as a google.maps.LatLng object. In this case,
-// we retrieve the marker's position using the
-// google.maps.LatLng.getPosition() method.
-// const infowindow = new google.maps.InfoWindow({
-//   content: "<p>Asset 1 Location:" + marker.getPosition() + "</p>",
-// });
+  // You can use a LatLng literal in place of a google.maps.LatLng object when
+  // creating the Marker object. Once the Marker object is instantiated, its
+  // position will be available as a google.maps.LatLng object. In this case,
+  // we retrieve the marker's position using the
+  // google.maps.LatLng.getPosition() method.
+  // const infowindow = new google.maps.InfoWindow({
+  //   content: "<p>Asset 1 Location:" + marker.getPosition() + "</p>",
+  // });
 
-// google.maps.event.addListener(marker, "click", () => {
-//   infowindow.open(map, marker);
-// });
+  // google.maps.event.addListener(marker, "click", () => {
+  //   infowindow.open(map, marker);
+  // });
 
-
+  
 
 
 // In this example, we center the map, and add a marker, using a LatLng object
@@ -61,7 +63,7 @@
 //     infowindow.open(map, marker);
 //   });
 //   if (latlngvalues!==oldlatlngvalues){
-
+  
 //     console.log("if chl gya h")
 //   animatedMove(marker,0.5,oldlatlngvalues,latlngvalues);
 // }
@@ -110,92 +112,90 @@
 // //}
 let icon = {
   url: "https://cdn-icons-png.flaticon.com/512/6395/6395539.png ",
-  scaledSize: {
-    width: 55,
-    height: 55
-  }
+  scaledSize: { width: 55, height: 55 }
 }
 
-var marker = [];
+var marker=[];
 var position = [0, 0];
-var marker_no = 0;
-var counter = 1;
-var socket = new WebSocket('ws://localhost:8000/ws/some_url/');
-
-socket.onmessage = function (event) {
-  var data = JSON.parse(event.data);
-  console.log(data);
-  if (counter == 1) {
-    for (i = 1; i <= Object.keys(data).length; i++) {
-      marker[i] = new google.maps.Marker({
-        position: new google.maps.LatLng(data[i][i - i], data[i][1]),
-        title: "Asset current location",
-        map: map,
-        icon: icon,
+var marker_no=0;
+var counter= 1;  
+var socket = new WebSocket('ws://localhost:8000/ws/tracking/'); 
+  socket.onmessage =  function(event){
+    var data = JSON.parse(event.data);
+    console.log(data);
+    if (counter ==1){
+    for (i=1;i<=Object.keys(data).length;i++){
+       marker[i]= new google.maps.Marker({
+        position :new google.maps.LatLng(data[i][i-i], data[i][1]),
+        title :""+new google.maps.LatLng(data[i][i-i], data[i][1]),
+        map : map,
+        icon :icon,
         optimized: false,
       }); //marker initial postion set
-      counter++;
+      counter ++ ;
+       };
+       
+      }
+      else{
+    
+      marker_no=Object.keys(data);
+      marker_no = marker_no[0];
+      console.log(data[marker_no]);
+      //console.log(data,{'lat':data[marker_no]['lat']});
+      latlngvalues ={'lat':data[marker_no]['lat'],'lng':data[marker_no]['lng']};
+      console.log("new value ",latlngvalues);
+      console.log("old value",marker[marker_no].getPosition().lat(),marker[marker_no].getPosition().lng());
+      transition(latlngvalues,marker_no);
     };
-
-  } else {
-
-    marker_no = Object.keys(data);
-    marker_no = marker_no[0];
-    //console.log(marker_no);
-    //console.log(data,{'lat':data[marker_no]['lat']});
-    latlngvalues = {
-      'lat': data[marker_no]['lat'],
-      'lng': data[marker_no]['lng']
+  };
+    
+    function initialize() {
+       
+        var latlng = new google.maps.LatLng(24.9812, 67.2313);
+        var myOptions = {
+            zoom: 8,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map"), myOptions);
     };
-    console.log("new value ", latlngvalues);
-    console.log("old value", marker[marker_no].getPosition().lat(), marker[marker_no].getPosition().lng());
-    transition(latlngvalues, marker_no);
-  };
-};
+    
+    var numDeltas = 1;
+    var delay = 10; //milliseconds
+    var i = 0;
+    var deltaLat;
+    var deltaLng;
 
-function initialize() {
-
-  var latlng = new google.maps.LatLng(24.9812, 67.2313);
-  var myOptions = {
-    zoom: 8,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("map"), myOptions);
-};
-
-var numDeltas = 100;
-var delay = 10; //milliseconds
-var i = 0;
-var deltaLat;
-var deltaLng;
-
-function transition(result, marker_no) {
-  //console.log(position); 
-  console.log("............", );
-  i = 0;
-
-  deltaLat = (result['lat'] - marker[marker_no].getPosition().lat()) / numDeltas;
-  deltaLng = (result['lng'] - marker[marker_no].getPosition().lng()) / numDeltas;
-  console.log(deltaLat, deltaLng);
-  moveMarker(marker_no);
-};
-
-function moveMarker(marker_no) {
-  lat = deltaLat + marker[marker_no].getPosition().lat();
-  lng = deltaLng + marker[marker_no].getPosition().lng();
-  //console.log(position[0], position[1]);
-  var latlng = new google.maps.LatLng(lat, lng);
-  //console.log(latlng);
-  //console.log(marker_no);
-  //console.log(marker[marker_no])  ;
-
-  marker[marker_no].setPosition(latlng);
-
-  if (i != numDeltas) {
-    i++;
-    console.log(i);
-    setTimeout(moveMarker, 10, marker_no);
-  };
-  //console.log(marker[marker_no].getPosition().lat(),marker[marker_no].getPosition().lng());
-};
+    function transition(result,marker_no){
+        //console.log(position); 
+        console.log("............",);
+        i=0;
+        
+        deltaLat = +(((result['lat'] - marker[marker_no].getPosition().lat())/numDeltas).toFixed(4));
+        
+        deltaLng = +(((result['lng'] - marker[marker_no].getPosition().lng())/numDeltas).toFixed(4));
+        console.log(deltaLat,deltaLng);
+        moveMarker(marker_no);
+    };
+    
+    function moveMarker(marker_no){
+      lat= +((deltaLat+marker[marker_no].getPosition().lat()).toFixed(4)) ;
+      lng=+((deltaLng+marker[marker_no].getPosition().lng()).toFixed(4));
+      console.log(lat,lng);
+        var latlng = new google.maps.LatLng(lat, lng);
+        //console.log(latlng);
+        //console.log(marker_no);
+        //console.log(marker[marker_no])  ;
+        
+        marker[marker_no].setPosition(latlng);
+        marker[marker_no].setTitle("Location"+latlng)
+        // if(i!=numDeltas){
+        //     i++;
+        //     console.log(i);
+        //     setTimeout(moveMarker,5,marker_no);
+        // };
+      
+        
+        //console.log(marker[marker_no].getPosition().lat(),marker[marker_no].getPosition().lng());
+    };
+    
