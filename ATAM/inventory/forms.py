9facb404e3d0fabb69181cdcb36b446bad2cca8e
Muhammad Farooq <45292795+Farooqut21@users.noreply.github.com>
2022-08-12 02:30:33 +0500

    Update forms.py

--- ATAM/inventory/forms.py
+++ ATAM/inventory/forms.py
@@ -11,10 +11,9 @@ class AddTrackerForm(forms.ModelForm):
     class Meta:
         model = Tracker
         fields= '__all__'
-<<<<<<< HEAD
-=======
+
 class AddJobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = '__all__'
->>>>>>> bd6d0e69e8d3e039c7e3007841949f112760e2e6
+
